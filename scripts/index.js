2bcc45cc87d12bbfc81c60c496c7f1ee74f0a167
Obito2912 <ovinsonlugo12@gmail.com>
2025-01-09 19:29:30 -0700

    Add transition to navBar when opening and add links

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,13 @@ const year = date.getFullYear();
 const lastModified = document.lastModified;
 const copyrightYear = document.querySelector('#year');
 const lastUpdate = document.querySelector('#last-update');
+const profilePic = document.querySelector('.js-profile-pic');
+const navBar = document.querySelector('.nav');
 
 copyrightYear.textContent = year;
 lastUpdate.textContent = lastModified;
+
+profilePic.addEventListener('click', () => {
+    profilePic.classList.toggle('rotate');
+    navBar.classList.toggle('js-open-navBar');
+});
