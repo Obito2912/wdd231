96e2f8a8689e1ade85163b09e6737a29fefceaa4
Obito2912 <ovinsonlugo12@gmail.com>
2025-01-09 21:58:30 -0700

    Change background color on svg when active and change footer's design as instructed in Canvas

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,13 @@
 const date = new Date();
 const year = date.getFullYear();
 const lastModified = document.lastModified;
-const copyrightYear = document.querySelector('#year');
-const lastUpdate = document.querySelector('#last-update');
+const copyrightYear = document.querySelector('#current-year');
+const lastUpdate = document.querySelector('#lastModified');
 const profilePic = document.querySelector('.js-profile-pic');
 const navBar = document.querySelector('.nav');
 
-copyrightYear.textContent = year;
-lastUpdate.textContent = lastModified;
+copyrightYear.innerHTML = `©${year}<br>`;
+lastUpdate.textContent = `Last update: ${lastModified}`;
 
 profilePic.addEventListener('click', () => {
     profilePic.classList.toggle('rotate');
