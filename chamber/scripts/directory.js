52734160dabd3dbb6c7a88c6a981e5fcbc02f79a
Obito2912 <ovinsonlugo12@gmail.com>
2025-01-19 13:05:36 -0700

    Make correction on the pic links on the json file and create two functions to display cards and fetch data

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -2,12 +2,43 @@ const navEl = document.querySelector('.navBar');
 const menuBtn = document.querySelector('.header__menu_icon');
 const year = document.querySelector('#current-year');
 const lastModified = document.querySelector('#last-modified');
+const mainSectionContainer = document.querySelector('.main__section_container');
+const chambersLink = 'https://obito2912.github.io/wdd231/chamber/data/members.json';
 const date = new Date;
 
 year.textContent += date.getFullYear();
 lastModified.textContent = document.lastModified;
 
+async function getChambersData() {
+    const response = await fetch(chambersLink);
+    const data = await response.json();
+    displayChamberCards(data);
+}
+
+function displayChamberCards(data) {
+
+    data.forEach(object => {
+        const section = document.createElement('section');
+        section.setAttribute('class', 'chamber__card');
+        section.innerHTML = 
+        `
+        <h3>${object.name}</h3>
+        <div>
+            <img src='${object.img}' alt='${object.name} image' width='80' height='80'>
+            <div>
+                <p>Address: ${object.address}</p>
+                <p>Phone Number: ${object.phoneNumber}</p>
+                <p>URL: ${object.url}</p>
+                <p>Membership: ${object.membership}</p>
+            </div>
+        </div>
+        `;
+        mainSectionContainer.append(section);
+    });
+}
 
 menuBtn.addEventListener('click', () => {
     navEl.classList.toggle('js-open-navBar');
 });
+
+getChambersData();
