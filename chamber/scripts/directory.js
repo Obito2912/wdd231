afffd5197dc6a7d927f288f14436dd97d6310170
Obito2912 <ovinsonlugo12@gmail.com>
2025-01-18 22:27:33 -0700

    Finish header and footer

---
+++ chamber/scripts/directory.js
@@ -0,0 +1,13 @@
+const navEl = document.querySelector('.navBar');
+const menuBtn = document.querySelector('.header__menu_icon');
+const year = document.querySelector('#current-year');
+const lastModified = document.querySelector('#last-modified');
+const date = new Date;
+
+year.textContent += date.getFullYear();
+lastModified.textContent += date.lastModified();
+
+
+menuBtn.addEventListener('click', () => {
+    navEl.classList.toggle('js-open-navBar');
+});
