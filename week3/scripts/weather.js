ed12440b7e3a92f4a2309191c51c60be06976587
Obito2912 <ovinsonlugo12@gmail.com>
2025-01-22 11:36:32 -0700

    Move files and organize according to course standards and create files for practicing with the weather API

---
+++ week3/scripts/weather.js
@@ -0,0 +1,30 @@
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const figCaption = document.querySelector('figcaption');
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=c917aec702bc6a2870617975229469d7';
+
+async function apiFetch() {
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            data = await response.json();
+        } else {
+            throw Error(await response.text());
+        }
+    } catch(error) {
+        console.log(error);
+    }
+    console.log(data);
+    displayResults(data);
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconSrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute('src', `${iconSrc}`);
+    weatherIcon.setAttribute('alt', 'Weather Icon Image');
+    figCaption.textContent = `${desc}`;
+  }
+
+apiFetch();
