d659948bc67c89c99c3c5295be24ae0e3fa48be4
Obito2912 <ovinsonlugo12@gmail.com>
2025-01-24 12:09:33 -0700

    Create different functions to get forecast and days. Adjust CSS on both pages and make the layout responsive

--- chamber/scripts/index.js
+++ chamber/scripts/index.js
@@ -1,21 +1,36 @@
 const weatherURL = 'https://api.openweathermap.org/data/2.5/weather?lat=41.04&lon=-111.94&units=imperial&appid=c917aec702bc6a2870617975229469d7';
-const weather5DayURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=41.04&lon=-111.94&units=imperial&appid=c917aec702bc6a2870617975229469d7';
+const forecastURL = 'https://api.openweathermap.org/data/2.5/forecast?lat=41.04&lon=-111.94&units=imperial&appid=c917aec702bc6a2870617975229469d7';
 const weatherIcon = document.querySelector('#weather-icon');
 const temp = document.querySelector('.temp');
 const desc = document.querySelector('.desc');
+let weekDays = getDynamicWeekDays();
 
 async function weatherData() {
     try {
-        const response = await fetch(weather5DayURL);
+        const response = await fetch(weatherURL);
         if (response.ok) {
             const data = await response.json();
-            // displayCurrentWeather(data);
-            // displayWeatherForecast(data);
+            displayCurrentWeather(data);
             console.log(data);
         } else {
             throw Error (await response.text());
         }
     } catch (error) {
+        console.log(error);
+    }
+}
+
+async function forecastData() {
+    try {
+        const response = await fetch(forecastURL);
+        if (response.ok) {
+            const data = await response.json();
+            displayWeatherForecast(weekDays, data);
+            console.log(data);
+        } else {
+            throw Error (await response.text());
+        }
+    } catch(error) {
         console.log(error);
     }
 }
@@ -27,19 +42,28 @@ function displayCurrentWeather(data) {
     desc.innerHTML += `${data.weather[0].description}`;
 }
 
-function displayWeatherForecast(data) {
-    const weatherForecast = document.querySelector('.weather-forecast');
+function displayWeatherForecast(weekDay, data) {
+    const weatherForecast = document.querySelector('.forecast div');
     const today = document.createElement('p');
     const tomorrow = document.createElement('p');
     const afterTomorrow = document.createElement('p');
 
-    today.textContent = `: `;
-    tomorrow.textContent = `: `;
-    afterTomorrow.textContent = `: `;
+    today.innerHTML = `<strong>${weekDay[0]}:</strong> ${data.list[0].main.temp}&deg;F`;
+    tomorrow.innerHTML = `<strong>${weekDay[1]}:</strong> ${data.list[8].main.temp}&deg;F`;
+    afterTomorrow.innerHTML = `<strong>${weekDay[2]}:</strong> ${data.list[16].main.temp}&deg;F`;
 
     weatherForecast.append(today);
     weatherForecast.append(tomorrow);
     weatherForecast.append(afterTomorrow);
 }
 
+function getDynamicWeekDays() {
+    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const today = new Date().getDay();
+
+    const dynamicDays = [...days.slice(today), ...days.slice(0, today)];
+    return dynamicDays;
+}
+
 weatherData();
+forecastData();
