adbcda32fd4ca6646bc74451e1899fde8f46fd48
Obito2912 <ovinsonlugo12@gmail.com>
2025-01-30 10:09:49 -0700

    Add some styles and make changes on the join page. Also add condition to header-footer js

--- chamber/scripts/header-footer.js
+++ chamber/scripts/header-footer.js
@@ -5,12 +5,13 @@ const lastModified = document.querySelector("#last-modified");
 const navLinks = document.querySelectorAll('.navBar__list_container li a');
 console.log(navLinks);
 const currentPage = window.location.pathname.split('/').pop();
+const currentPageName = currentPage.split('.');
 const date = new Date();
 year.textContent += date.getFullYear();
 lastModified.textContent = document.lastModified;
 
 navLinks.forEach(link => {
-  if(link.getAttribute('href') === currentPage || link.getAttribute('href') === `${currentPage}#`) {
+  if(link.getAttribute('href') === currentPage || link.getAttribute('href') === `${currentPage}#` || link.getAttribute('href') === currentPageName[0]) {
     link.classList.add('js-active');
   }
 });
