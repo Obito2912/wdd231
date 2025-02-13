5d51ec5b9d5349eca357822b1f0df46fdec87ea4
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2025-02-12 20:00:06 -0700

    Add animation to icon with css and JS

--- animeptm/scripts/index.js
+++ animeptm/scripts/index.js
@@ -1,4 +1,15 @@
 const menuIconContainer = document.querySelector('.menu-icon__container');
+const menuIcon = document.querySelector('.menu-icon');
 const navMenu = document.querySelector('.nav-menu');
 
-menuIconContainer.addEventListener('click', () => navMenu.classList.toggle('toggle-open-menu'));
+menuIconContainer.addEventListener('click', () => {
+    navMenu.classList.toggle('toggle-open-menu');
+    
+    if (!menuIcon.classList.contains('rotate')) {
+        menuIcon.classList.remove('rotate-back');
+        menuIcon.classList.add('rotate');
+    } else {
+        menuIcon.classList.remove('rotate');
+        menuIcon.classList.add('rotate-back');
+    }
+});
