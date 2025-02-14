04aae1864a6d8995af9b313a45ba047f8c6c8a66
Obito2912 <https://obito2912.github.io/se_project_aroundtheus/>
2025-02-13 20:35:09 -0700

    Update JS to go according with the design

--- animeptm/scripts/index.js
+++ animeptm/scripts/index.js
@@ -38,30 +38,39 @@ function createInfoCard(data, index) {
 }
 
 function doNotShow(data, index) {
-    const rating = data[index].rating;
-    if (rating && rating.length >= 2) {
-        const firstTwoLetters = rating.substring(0, 2) // I could also use slice
-        if (firstTwoLetters === 'R+' || firstTwoLetters === 'Rx') {
-            return true;
-        }
+  const rating = data[index].rating;
+  if (rating && rating.length >= 2) {
+    const firstTwoLetters = rating.substring(0, 2); // I could also use slice
+    if (firstTwoLetters === "R+" || firstTwoLetters === "Rx") {
+      return true;
     }
+  }
 }
 
 function displayData(data) {
-    const startIndex = 0;
-    const maxCards = 15;
-    const endIndex = Math.min(startIndex + maxCards, data.length);
+  const maxCards = 15;
+  let startIndex = 0;
+  let cardsCreated = 0;
 
-
-
-  for (let index = startIndex; index < data.length; index++) {
-    if (data[index] && data[index].trailer && data[index].trailer.embed_url && data[index].synopsis) {
-      
-      if (!doNotShow(data, index)) {
+  do {
+    for (let index = startIndex; index < data.length; index++) {
+      if (
+        data[index] &&
+        data[index].trailer &&
+        data[index].trailer.embed_url &&
+        data[index].synopsis
+      ) {
+        if (!doNotShow(data, index)) {
           createInfoCard(data, index);
+          cardsCreated++;
+          if (cardsCreated >= maxCards) {
+            break;
+          }
+        }
       }
     }
-  }
+    startIndex++;
+  } while (cardsCreated < maxCards && startIndex < data.length);
 }
 
 getAnimeData();
