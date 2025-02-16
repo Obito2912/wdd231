da282f31263ee4af1003cfa5ed33b390e38e55c6
Obito2912 <ovinsonlugo12@gmail.com>
2025-02-16 00:10:35 -0700

    Convert images to webp

--- animeptm/data/images.mjs
+++ animeptm/data/images.mjs
@@ -1,158 +1,145 @@
 export const animeList = [
   {
     title: "Attack on Titan",
-    src: "https://example.com/attackontitan.jpg",
     genre: ["Action", "Drama", "Fantasy"],
     images: [
-      "attack-on-titan1",
-      "attack-on-titan2",
-      "attack-on-titan3",
-      "attack-on-titan4",
-      "attack-on-titan5",
+      "attack-on-titan1.webp",
+      "attack-on-titan2.webp",
+      "attack-on-titan3.webp",
+      "attack-on-titan4.webp",
+      "attack-on-titan5.webp",
     ],
   },
   {
     title: "Demon Slayer",
-    src: "https://example.com/demonslayer.jpg",
     genre: ["Action", "Fantasy"],
     images: [
-        "demon-slayer1",
-        "demon-slayer2",
-        "demon-slayer3",
-        "demon-slayer4",
-        "demon-slayer5"
+        "demon-slayer1.webp",
+        "demon-slayer2.webp",
+        "demon-slayer3.webp",
+        "demon-slayer4.webp",
+        "demon-slayer5.webp"
     ],
   },
   {
     title: "One Piece",
-    src: "https://example.com/onepiece.jpg",
     genre: ["Action", "Adventure"],
     images: [
-        "one-piece1",
-        "one-piece2",
-        "one-piece3",
-        "one-piece4",
-        "one-piece5"
+        "one-piece1.webp",
+        "one-piece2.webp",
+        "one-piece3.webp",
+        "one-piece4.webp",
+        "one-piece5.webp"
     ],
   },
   {
     title: "Jujutsu Kaisen",
-    src: "https://example.com/jujutsukaisen.jpg",
     genre: ["Action", "Fantasy"],
     images: [
-        "jujutsu-kaisen1",
-        "jujutsu-kaisen2",
-        "jujutsu-kaisen3",
-        "jujutsu-kaisen4",
-        "jujutsu-kaisen5"
+        "jujutsu-kaisen1.webp",
+        "jujutsu-kaisen2.webp",
+        "jujutsu-kaisen3.webp",
+        "jujutsu-kaisen4.webp",
+        "jujutsu-kaisen5.webp"
     ],
   },
   {
     title: "My Hero Academia",
-    src: "https://example.com/myheroacademia.jpg",
     genre: ["Action", "Adventure"],
     images: [
-        "my-hero-academy1",
-        "my-hero-academy2",
-        "my-hero-academy3",
-        "my-hero-academy4",
-        "my-hero-academy5"
+        "my-hero-academy1.webp",
+        "my-hero-academy2.webp",
+        "my-hero-academy3.webp",
+        "my-hero-academy4.webp",
+        "my-hero-academy5.webp"
     ],
   },
   {
     title: "Naruto",
-    src: "https://example.com/naruto.jpg",
     genre: ["Action", "Adventure"],
     images: [
-        "naruto1",
-        "naruto2",
-        "naruto3",
-        "naruto4",
-        "naruto5"
+        "naruto1.webp",
+        "naruto2.webp",
+        "naruto3.webp",
+        "naruto4.webp",
+        "naruto5.webp"
     ],
   },
   {
     title: "Death Note",
-    src: "https://example.com/deathnote.jpg",
     genre: ["Mystery", "Thriller"],
     images: [
-        "death-note1",
-        "death-note2",
-        "death-note3",
-        "death-note4",
-        "death-note5"
+        "death-note1.webp",
+        "death-note2.webp",
+        "death-note3.webp",
+        "death-note4.webp",
+        "death-note5.webp"
     ],
   },
   {
     title: "Fullmetal Alchemist: Brotherhood",
-    src: "https://example.com/fullmetalalchemist.jpg",
     genre: ["Action", "Fantasy"],
     images: [
-        "fullmetal-alchemist1",
-        "fullmetal-alchemist2",
-        "fullmetal-alchemist3",
-        "fullmetal-alchemist4",
-        "fullmetal-alchemist5"
+        "fullmetal-alchemist1.webp",
+        "fullmetal-alchemist2.webp",
+        "fullmetal-alchemist3.webp",
+        "fullmetal-alchemist4.webp",
+        "fullmetal-alchemist5.webp"
     ],
   },
   {
     title: "Hunter x Hunter",
-    src: "https://example.com/hunterxhunter.jpg",
     genre: ["Action", "Adventure"],
     images: [
-        "hunter-x-hunter1",
-        "hunter-x-hunter2",
-        "hunter-x-hunter3",
-        "hunter-x-hunter4",
-        "hunter-x-hunter5"
+        "hunter-x-hunter1.webp",
+        "hunter-x-hunter2.webp",
+        "hunter-x-hunter3.webp",
+        "hunter-x-hunter4.webp",
+        "hunter-x-hunter5.webp"
     ],
   },
   {
     title: "One Punch Man",
-    src: "https://example.com/onepunchman.jpg",
     genre: ["Action", "Comedy"],
     images: [
-        "one-punch-man1",
-        "one-punch-man2",
-        "one-punch-man3",
-        "one-punch-man4",
-        "one-punch-man5"
+        "one-punch-man1.webp",
+        "one-punch-man2.webp",
+        "one-punch-man3.webp",
+        "one-punch-man4.webp",
+        "one-punch-man5.webp"
     ],
   },
   {
     title: "Sword Art Online",
-    src: "https://example.com/swordartonline.jpg",
     genre: ["Action", "Adventure"],
     images: [
-        "sword-art-online1",
-        "sword-art-online2",
-        "sword-art-online3",
-        "sword-art-online4",
-        "sword-art-online5"
+        "sword-art-online1.webp",
+        "sword-art-online2.webp",
+        "sword-art-online3.webp",
+        "sword-art-online4.webp",
+        "sword-art-online5.webp"
     ],
   },
   {
     title: "Tokyo Ghoul",
-    src: "https://example.com/tokyoghoul.jpg",
     genre: ["Action", "Horror"],
     images: [
-        "tokyo-ghoul1",
-        "tokyo-ghoul2",
-        "tokyo-ghoul3",
-        "tokyo-ghoul4",
-        "tokyo-ghoul5"
+        "tokyo-ghoul1.webp",
+        "tokyo-ghoul2.webp",
+        "tokyo-ghoul3.webp",
+        "tokyo-ghoul4.webp",
+        "tokyo-ghoul5.webp"
     ],
   },
   {
     title: "Parasyte",
-    src: "https://example.com/parasyte.jpg",
     genre: ["Action", "Horror"],
     images: [
-        "parasyte1",
-        "parasyte2",
-        "parasyte3",
-        "parasyte4",
-        "parasyte5"
+        "parasyte1.webp",
+        "parasyte2.webp",
+        "parasyte3.webp",
+        "parasyte4.webp",
+        "parasyte5.webp"
     ],
   },
 ];
--- animeptm/scripts/gallery.js
+++ animeptm/scripts/gallery.js
@@ -1,12 +1,31 @@
 import { animeList } from "../data/images.mjs";
 
-const main = document.querySelector('.gallery-main');
+const animeInfo = animeList;
+const imagesContainer = document.querySelector(".images__container");
+const imagesContainerDiv = document.querySelector('.images__container div');
+const path = "images/";
 
-function createCard(image) {
-    main.innerHTML = `
-    <div>
-        <img src='' width='' height='' loading='lazy'>
-        <h2>${image}</h2>
+function displayCards(info) {
+  let objectIndex = 0;
+  let imageIndex = 0;
+  info.forEach((element) => {
+    createCard(element);
+    objectIndex++;
+    element.images.forEach((image) => {
+      imageIndex++;
+      imagesContainerDiv.innerHTML += `
+        <img src='${path}${image}' alt='${image} image' width='200' height='200' loading='lazy'>
+        `;
+    });
+  });
+}
+
+function createCard(info) {
+  imagesContainer.innerHTML += `
+    <div class='card__content>
+        <h2>${info.title}</h2>
     </div>
     `;
 }
+
+displayCards(animeInfo);
